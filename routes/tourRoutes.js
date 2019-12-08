b964af2ff879b1dacb48b2f02c9acfd45d9cf300
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-12-08 15:28:18 +0530

    API to get distances

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -25,6 +25,12 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
+router
   .route('/')
   .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
